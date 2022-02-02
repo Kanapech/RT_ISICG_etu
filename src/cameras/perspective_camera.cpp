e73799dac44536a1d8be9799a5b6a2569d3bf296
Kanapech <silver36tv@gmail.com>
2022-02-02 17:21:55 +0100

    Add project files.

---
+++ src/cameras/perspective_camera.cpp
@@ -0,0 +1,31 @@
+#include "perspective_camera.hpp"
+#include <glm/gtx/string_cast.hpp>
+
+namespace RT_ISICG
+{
+	PerspectiveCamera::PerspectiveCamera( const float p_aspectRatio ) : _aspectRatio( p_aspectRatio )
+	{
+		_updateViewport();
+	}
+
+	PerspectiveCamera::PerspectiveCamera( const Vec3f & p_position,
+										  const Vec3f & p_lookAt,
+										  const Vec3f & p_up,
+										  const float	p_fovy,
+										  const float	p_aspectRatio )
+		: BaseCamera( p_position ), _fovy( p_fovy ), _aspectRatio( p_aspectRatio )
+	{
+		/// TODO ! _u ? _v ? _w ?
+
+		_updateViewport();
+	}
+
+	void PerspectiveCamera::_updateViewport()
+	{
+		/// TODO ! _viewportTopLeftCorner ?	_viewportU ? _viewportV ?
+		float h	   = glm::tan( glm::radians( _fovy ) ) * _focalDistance;
+		_viewportU = ( h * _aspectRatio ) * _u;
+		_viewportV = h * _v;
+	}
+
+} // namespace RT_ISICG
